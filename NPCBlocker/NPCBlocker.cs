d7461bd69199998f273d6175877ba3ffca18b1f3
Patrikk <patrikveres@hotmail.com>
2015-07-08 09:02:50 +0100

    Fix ugly spacing to pretty tabbing.

--- NPCBlocker/NPCBlocker.cs
+++ NPCBlocker/NPCBlocker.cs
@@ -25,194 +25,194 @@ using Terraria;
 
 namespace NPCBlocker
 {
-    [ApiVersion(1, 18)]
-    public class NPCBlocker : TerrariaPlugin
-    {
-        private IDbConnection db;
-        private List<int> blockedNPC = new List<int>();
-        public override Version Version
-        {
-            get { return new Version(1,10); }
-        }
+	[ApiVersion(1, 18)]
+	public class NPCBlocker : TerrariaPlugin
+	{
+		private IDbConnection db;
+		private List<int> blockedNPC = new List<int>();
+		public override Version Version
+		{
+			get { return new Version(1,10); }
+		}
 
-        public override string Name
-        {
-            get { return "NPC Blocker"; }
-        }
+		public override string Name
+		{
+			get { return "NPC Blocker"; }
+		}
 
-        public override string Author
-        {
-            get { return "Olink"; }
-        }
+		public override string Author
+		{
+			get { return "Olink"; }
+		}
 
-        public override string Description
-        {
-            get { return "Blocks npcs from being spawned."; }
-        }
+		public override string Description
+		{
+			get { return "Blocks npcs from being spawned."; }
+		}
 
-        public NPCBlocker(Main game)
-            : base(game)
-        {
-            Order = 4;
-        }
+		public NPCBlocker(Main game)
+			: base(game)
+		{
+			Order = 4;
+		}
 
-        public override void Initialize()
-        {
-            TShockAPI.Commands.ChatCommands.Add(new Command("resnpc", AddNpc, "blacknpc"));
-            TShockAPI.Commands.ChatCommands.Add(new Command("resnpc", DelNpc, "whitenpc"));
-            ServerApi.Hooks.NpcSpawn.Register(this, OnSpawn);
+		public override void Initialize()
+		{
+			TShockAPI.Commands.ChatCommands.Add(new Command("resnpc", AddNpc, "blacknpc"));
+			TShockAPI.Commands.ChatCommands.Add(new Command("resnpc", DelNpc, "whitenpc"));
+			ServerApi.Hooks.NpcSpawn.Register(this, OnSpawn);
 			ServerApi.Hooks.NpcTransform.Register(this, OnTransform);
-            StartDB();
-        }
+			StartDB();
+		}
 
-        public void StartDB()
-        {
-            SetupDb();
-            ReadDb();
-        }
+		public void StartDB()
+		{
+			SetupDb();
+			ReadDb();
+		}
 
-        private void SetupDb()
-        {
-            if (TShock.Config.StorageType.ToLower() == "sqlite")
-            {
-                string sql = Path.Combine(TShock.SavePath, "npc_blocker.sqlite");
-                db = new SqliteConnection(string.Format("uri=file://{0},Version=3", sql));
-            }
-            else if (TShock.Config.StorageType.ToLower() == "mysql")
-            {
-                try
-                {
-                    var hostport = TShock.Config.MySqlHost.Split(':');
-                    db = new MySqlConnection();
-                    db.ConnectionString =
-                        String.Format("Server={0}; Port={1}; Database={2}; Uid={3}; Pwd={4};",
-                                      hostport[0],
-                                      hostport.Length > 1 ? hostport[1] : "3306",
-                                      TShock.Config.MySqlDbName,
-                                      TShock.Config.MySqlUsername,
-                                      TShock.Config.MySqlPassword
-                            );
-                }
-                catch (MySqlException ex)
-                {
-                    TShock.Log.Error(ex.ToString());
-                    throw new Exception("MySql not setup correctly");
-                }
-            }
-            else
-            {
-                throw new Exception("Invalid storage type");
-            }
-            
-            var table2 = new SqlTable("Blocked_NPC",
-                                     new SqlColumn("ID", MySqlDbType.Int32)
-                );
-            var creator2 = new SqlTableCreator(db,
-                                              db.GetSqlType() == SqlType.Sqlite
-                                                ? (IQueryBuilder)new SqliteQueryCreator()
-                                                : new MysqlQueryCreator());
-            creator2.EnsureTableStructure(table2);
-        }
+		private void SetupDb()
+		{
+			if (TShock.Config.StorageType.ToLower() == "sqlite")
+			{
+				string sql = Path.Combine(TShock.SavePath, "npc_blocker.sqlite");
+				db = new SqliteConnection(string.Format("uri=file://{0},Version=3", sql));
+			}
+			else if (TShock.Config.StorageType.ToLower() == "mysql")
+			{
+				try
+				{
+					var hostport = TShock.Config.MySqlHost.Split(':');
+					db = new MySqlConnection();
+					db.ConnectionString =
+						String.Format("Server={0}; Port={1}; Database={2}; Uid={3}; Pwd={4};",
+									  hostport[0],
+									  hostport.Length > 1 ? hostport[1] : "3306",
+									  TShock.Config.MySqlDbName,
+									  TShock.Config.MySqlUsername,
+									  TShock.Config.MySqlPassword
+							);
+				}
+				catch (MySqlException ex)
+				{
+					TShock.Log.Error(ex.ToString());
+					throw new Exception("MySql not setup correctly");
+				}
+			}
+			else
+			{
+				throw new Exception("Invalid storage type");
+			}
+			
+			var table2 = new SqlTable("Blocked_NPC",
+									 new SqlColumn("ID", MySqlDbType.Int32)
+				);
+			var creator2 = new SqlTableCreator(db,
+											  db.GetSqlType() == SqlType.Sqlite
+												? (IQueryBuilder)new SqliteQueryCreator()
+												: new MysqlQueryCreator());
+			creator2.EnsureTableStructure(table2);
+		}
 
-        private void ReadDb()
-        {
-            String query = "SELECT * FROM Blocked_NPC";
+		private void ReadDb()
+		{
+			String query = "SELECT * FROM Blocked_NPC";
 
-            var reader = db.QueryReader(query);
+			var reader = db.QueryReader(query);
 	
 			while (reader.Read())
 			{
-                blockedNPC.Add(reader.Get<int>("ID"));
+				blockedNPC.Add(reader.Get<int>("ID"));
 			}
-        }
+		}
 
-        private void AddNpc(CommandArgs args)
-        {
-            if (args.Parameters.Count < 1)
-            {
-                args.Player.SendMessage("You must specify a npc id to add.", Color.Red);
-                return;
-            }
-            string tile = args.Parameters[0];
-            int id;
-            if (!int.TryParse(tile, out id))
-            {
-                args.Player.SendMessage(String.Format("Npc id '{0}' is not a valid number.", id), Color.Red);
-                return;
-            }
+		private void AddNpc(CommandArgs args)
+		{
+			if (args.Parameters.Count < 1)
+			{
+				args.Player.SendMessage("You must specify a npc id to add.", Color.Red);
+				return;
+			}
+			string tile = args.Parameters[0];
+			int id;
+			if (!int.TryParse(tile, out id))
+			{
+				args.Player.SendMessage(String.Format("Npc id '{0}' is not a valid number.", id), Color.Red);
+				return;
+			}
 
-            String query = "INSERT INTO Blocked_NPC (ID) VALUES (@0);";
+			String query = "INSERT INTO Blocked_NPC (ID) VALUES (@0);";
 
-            if (db.Query(query, id) != 1)
-            {
-                TShock.Log.ConsoleError("Inserting into the database has failed!");
-                args.Player.SendMessage(String.Format("Inserting into the database has failed!", id), Color.Red);
-            }
-            else
-            {
-                args.Player.SendMessage(String.Format("Successfully banned {0}", id), Color.Red);
-                blockedNPC.Add(id);
-            }
-        }
+			if (db.Query(query, id) != 1)
+			{
+				TShock.Log.ConsoleError("Inserting into the database has failed!");
+				args.Player.SendMessage(String.Format("Inserting into the database has failed!", id), Color.Red);
+			}
+			else
+			{
+				args.Player.SendMessage(String.Format("Successfully banned {0}", id), Color.Red);
+				blockedNPC.Add(id);
+			}
+		}
 
-        private void DelNpc(CommandArgs args)
-        {
-            if (args.Parameters.Count < 1)
-            {
-                args.Player.SendMessage("You must specify a npc id to remove.", Color.Red);
-                return;
-            }
-            string tile = args.Parameters[0];
-            int id;
-            if (!int.TryParse(tile, out id))
-            {
-                args.Player.SendMessage(String.Format("Npc id '{0}' is not a valid number.", id), Color.Red);
-                return;
-            }
-            String query = "DELETE FROM Blocked_NPC WHERE ID = @0;";
+		private void DelNpc(CommandArgs args)
+		{
+			if (args.Parameters.Count < 1)
+			{
+				args.Player.SendMessage("You must specify a npc id to remove.", Color.Red);
+				return;
+			}
+			string tile = args.Parameters[0];
+			int id;
+			if (!int.TryParse(tile, out id))
+			{
+				args.Player.SendMessage(String.Format("Npc id '{0}' is not a valid number.", id), Color.Red);
+				return;
+			}
+			String query = "DELETE FROM Blocked_NPC WHERE ID = @0;";
 
-            if (db.Query(query, id) != 1)
-            {
-                TShock.Log.ConsoleError("Removing from the database has failed!");
-                args.Player.SendMessage(String.Format("Removing from the database has failed!  Are you sure {0} is banned?", id), Color.Red);
-            }
-            else
-            {
-                args.Player.SendMessage(String.Format("Successfully unbanned {0}", id), Color.Green);
-                blockedNPC.Remove(id);
-            }
-        }
+			if (db.Query(query, id) != 1)
+			{
+				TShock.Log.ConsoleError("Removing from the database has failed!");
+				args.Player.SendMessage(String.Format("Removing from the database has failed!  Are you sure {0} is banned?", id), Color.Red);
+			}
+			else
+			{
+				args.Player.SendMessage(String.Format("Successfully unbanned {0}", id), Color.Green);
+				blockedNPC.Remove(id);
+			}
+		}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
 				ServerApi.Hooks.NpcSpawn.Deregister(this, OnSpawn);
-            }
+			}
 
-            base.Dispose(disposing);
-        }
+			base.Dispose(disposing);
+		}
 
-	    private void OnTransform(NpcTransformationEventArgs args)
-	    {
+		private void OnTransform(NpcTransformationEventArgs args)
+		{
 			if (args.Handled)
 				return;
 			if (blockedNPC.Contains(Main.npc[args.NpcId].netID))
 			{
 				Main.npc[args.NpcId].active = false;
 			}
-	    }
+		}
 
-        private void OnSpawn( NpcSpawnEventArgs args)
-        {
-            if (args.Handled)
-                return;
+		private void OnSpawn( NpcSpawnEventArgs args)
+		{
+			if (args.Handled)
+				return;
 			if (blockedNPC.Contains(Main.npc[args.NpcId].netID))
 			{
 				args.Handled = true;
 				Main.npc[args.NpcId].active = false;
-	            args.NpcId = 200;
-            }
-        }
-    }
+				args.NpcId = 200;
+			}
+		}
+	}
 }
